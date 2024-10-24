a92a0eab6a5a440fca147bf1f9bb7fd11c460847
Daniel Beltejar <contacto@danielbeltejar.es>
2024-10-25 00:29:12 +0200

    Updated module: tfmod-kubernetes-namespaces | Date: 2024-10-25 00:29:08

--- tfmod-kubernetes-namespaces/locals.tf
+++ tfmod-kubernetes-namespaces/locals.tf
@@ -1,7 +1,7 @@
 locals {
   namespaces_with_defaults = [
     for ns in var.namespaces : merge(ns, {
-      automount_service_account = lookup(ns, "automount_service_account", true)
+      automount_service_account = lookup(ns, "automount_service_account", false)
     })
   ]
 }
