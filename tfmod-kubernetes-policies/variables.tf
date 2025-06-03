5f4d09f44a3d68528dd735e3eadc72988f135622
Daniel beltejar <contacto@danielbeltejar.es>
2025-06-03 20:55:42 +0200

    test

--- tfmod-kubernetes-policies/variables.tf
+++ tfmod-kubernetes-policies/variables.tf
@@ -2,7 +2,7 @@ variable "policies" {
   type = map(object({
     name                = string
     policy              = string
-    api_groups          = optional(list(string), "[]")
+    api_groups          = optional(string, "[]")
     kinds               = optional(list(string), ["Pod"])
     excluded_namespaces = optional(list(string), ["kube-*", "*-system", "cert-manager", "ingress-nginx", "*-pihole", "*-jenkins", "monitoring", "lab-wireguard", "lab-openclarity"])
   }))
