a69a474febb38b48a5ae16e198dbf664354f1bd0
Daniel beltejar <contacto@danielbeltejar.es>
2025-06-26 12:22:15 +0200

    Updated module: tfmod-kubernetes-policies | Date: 2025-06-26 12:22:15

--- tfmod-kubernetes-policies/variables.tf
+++ tfmod-kubernetes-policies/variables.tf
@@ -4,7 +4,7 @@ variable "policies" {
     policy              = string
     api_groups          = optional(list(string), [])
     kinds               = optional(list(string), ["Pod"])
-    excluded_namespaces = optional(list(string), ["kube-*", "*-system", "cert-manager", "ingress-nginx", "*-pihole", "*-jenkins", "monitoring", "lab-wireguard", "lab-openclarity"])
+    excluded_namespaces = optional(list(string), ["kube-*", "*-system", "cert-manager", "ingress-nginx", "*-pihole", "*-jenkins", "monitoring", "lab-wireguard", "lab-openclarity", "ollama"])
   }))
 }
 
